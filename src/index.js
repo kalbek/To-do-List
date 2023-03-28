2533c35d2cb35060929d81ab157e36fc79b84e07
kalbek <access2kaleb@gmail.com>
2023-03-28 21:10:10 +0300

    create To-do lists dynamically

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 // import _ from 'lodash';
-import './style.css';
+import "./style.css";
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
+  const todoList = document.querySelector('.list-container')
+  const tasks = [
+    {
+      index: 0,
+      description: "wash the dishes",
+      completed: "false",
+    },
+    {
+      index: 1,
+      description: "complete To Do list project",
+      completed: "false",
+    },
+  ];
 
+  tasks.forEach(task => {
+    todoList.innerHTML += `
+    <section class="lists">
+      <div class="list">
+        <input type="checkbox" id="single-list-${task.index}" />
+        <label for="single-list-${task.index}">${task.description} </label>
+      </div>
+      <i id="selection" class="ptr"></i>
+    </section>
+    `
+  })
   // Lodash, now imported by this script
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   // element.classList.add('hello');
