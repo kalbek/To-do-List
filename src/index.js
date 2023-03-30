a3062ba9d76e04aa42ec80ec56a397716e039fb2
kalbek <access2kaleb@gmail.com>
2023-03-30 13:44:21 +0300

    implement removing todos by index

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 // import _ from 'lodash';
-import "./style.css";
-import Todo from "./modules/Todo";
-const addTodo = document.querySelector("#enter");
-const todoList = document.querySelector(".list-container");
-const todo = document.querySelector("#todo-input");
+import './style.css';
+import Todo from './modules/Todo.js';
+
+const addTodo = document.querySelector('#enter');
+const todoList = document.querySelector('.list-container');
+const todo = document.querySelector('#todo-input');
 
 function addTodos() {
-  if (todo.value !== "") Todo.setTodo(todo.value, false);
-  else alert("Todos cannot be empty!");
-  todo.value = "";
+  if (todo.value !== '') Todo.setTodo(todo.value, false);
+
+  todo.value = '';
   todo.focus();
 }
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
   Todo.updateUI(todoList);
 
   // handle event for add todo
-  addTodo.addEventListener("click", () => {
+  addTodo.addEventListener('click', () => {
     addTodos();
   });
-  todo.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") addTodos();
+
+  todo.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') addTodos();
   });
 
   return element;
 }
 
-window.onload = () => {};
+window.onload = () => {
+  todo.focus();
+};
 document.body.appendChild(component());
--- src/modules/Todo.js
+++ src/modules/Todo.js
@@ -1,46 +1,56 @@
 export default class Todo {
   static todoList = [];
+
   constructor(description, completed) {
     this.description = description;
     this.completed = completed;
-    this.index = Todo.length;
+    this.index = Todo.todoList.length;
   }
+
+  // add todo into Todo's todoList
   static setTodo = (description, completed) => {
     const todo = new Todo(description, completed);
-    const todoList = document.querySelector(".list-container");
+    const todoList = document.querySelector('.list-container');
     Todo.todoList.push(todo);
-    console.log("af: ", Todo.todoList);
     Todo.updateUI(todoList);
   };
 
-  static getTodo = (index) => {
-    todos.forEach((todo) => {
-      if (todo.index === index) {
-        return todo;
-      }
+  // remove todo from Todo's todoList by index
+  static removeTodo = (index) => {
+    const todoList = document.querySelector('.list-container');
+    Todo.todoList = Todo.todoList.filter((toDo) => toDo.index !== index);
+    // update id's of remaining todo's
+    Todo.todoList.forEach((todo) => {
+      if (todo.index > index) todo.index -= 1;
     });
-    return null;
+    Todo.updateUI(todoList);
+    // foucs carret on input box
+    document.querySelector('#todo-input').focus();
   };
-
-  static getTodos = () => {
-    return Todo;
-  };
-
-  static removeTodo = () => {};
 
   static updateUI = (targetElement) => {
-    //   first clear the UI at target
-    targetElement.innerHTML = "";
-    Todo.todoList.forEach((task) => {
+    //  clear currently displayed todos
+    targetElement.innerHTML = '';
+    // update to do lists display with current todoList
+    for (let i = Todo.todoList.length - 1; i >= 0; i -= 1) {
+      const task = Todo.todoList[i];
       targetElement.innerHTML += `
-        <section class="lists">
-            <div class="list">
-                <input class='single-todo' type="checkbox" id="single-list-${task.index}" />
-                <label for="single-list-${task.index}">${task.description} </label>
+        <section class='lists'>
+            <div class='list'>
+                <input class='single-todo' type='checkbox' id='single-list-${task.index}' />
+                <label for='single-list-${task.index}'>${task.description} </label>
             </div>
-            <i id="selection" class="ptr"></i>
+            <i id='selection' class='ptr remove-${i}'></i>
         </section>
     `;
+    }
+    // for each newly displayed to does create remove event
+    Todo.todoList.forEach((_, index) => {
+      const remove = document.querySelector(`.remove-${index}`);
+      remove.addEventListener('click', () => {
+        const indexToRemove = Number(remove.classList[1].slice(7));
+        Todo.removeTodo(indexToRemove);
+      });
     });
   };
 }
